d655b18b80b3e96add026ab4810eafc675bb50be
vypham <uty.tra@thebargainvillage.com>
2024-06-07 10:16:13 +0700

    message

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ class Config:
         github_config = self.config.get("github", {})
         return (
             github_config["repo"],
-            github_config["token"],
+            os.getenv("GITHUB_TOKEN"),  # Read the token from environment variable
             github_config["workflow_id"],
             github_config["branch"],
         )
