338b05d9c8c14124eebea13dcf1c5b147864c55a
vypham <uty.tra@thebargainvillage.com>
2024-07-05 17:10:23 +0700

    message

--- ai_amazon_keyword.py
+++ ai_amazon_keyword.py
@@ -93,7 +93,7 @@ def scrap_amazon_keyword(driver, df_keywords):
             driver.get(f"https://www.amazon.com/s?k={data_item['synonyms_keyword']}")
 
             # Wait for the search input field to be visible
-            search_box = WebDriverWait(driver, 100000000).until(
+            search_box = WebDriverWait(driver, 30).until(
                 EC.visibility_of_element_located((By.ID, "twotabsearchtextbox"))
             )
 
