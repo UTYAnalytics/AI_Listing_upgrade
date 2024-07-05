f78de92fdc4df2838a2f5445c35e669779b47d8a
vypham <uty.tra@thebargainvillage.com>
2024-07-05 17:36:58 +0700

    message

--- ai_amazon_keyword.py
+++ ai_amazon_keyword.py
@@ -90,7 +90,7 @@ def scrap_amazon_keyword(driver, df_keywords):
         datas_keyword = pd.DataFrame(keyword_list)
 
         for index, data_item in datas_keyword.iterrows():
-            driver.get(f"https://www.amazon.com/s?k={data_item['synonyms_keyword']}")
+            driver.get(f"https://www.amazon.com/ref=nav_logo")
             print(
                 f"Navigated to https://www.amazon.com/s?k={data_item['synonyms_keyword']}"
             )
